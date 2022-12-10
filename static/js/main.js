b57498ca301479659155f67b88a4b2ca760b5d0e
烟花小神 <1347229508@qq.com>
2022-12-11 00:49:34 +0800

    富平柿饼出 Q 1347229508

---
+++ static/js/main.js
@@ -0,0 +1,67 @@
+(function($){
+    $.fn.scrollingTo = function( opts ) {
+        var defaults = {
+            animationTime : 1000,
+            easing : '',
+            callbackBeforeTransition : function(){},
+            callbackAfterTransition : function(){}
+        };
+
+        var config = $.extend( {}, defaults, opts );
+
+        $(this).click(function(e){
+            var eventVal = e;
+            e.preventDefault();
+
+            var $section = $(document).find( $(this).data('section') );
+            if ( $section.length < 1 ) {
+                return false;
+            };
+
+            if ( $('html, body').is(':animated') ) {
+                $('html, body').stop( true, true );
+            };
+
+            var scrollPos = $section.offset().top;
+
+            if ( $(window).scrollTop() == scrollPos ) {
+                return false;
+            };
+
+            config.callbackBeforeTransition(eventVal, $section);
+
+            $('html, body').animate({
+                'scrollTop' : (scrollPos+'px' )
+            }, config.animationTime, config.easing, function(){
+                config.callbackAfterTransition(eventVal, $section);
+            });
+        });
+    };
+}(jQuery));
+
+
+if (screen.width<767){
+    wow = new WOW(
+  {
+    animateClass: 'animated',
+    offset:0
+  }
+);
+wow.init();
+}else{
+    wow = new WOW(
+  {
+    animateClass: 'animated',
+    offset:0
+  }
+);
+wow.init();
+}
+
+
+
+
+
+
+
+
